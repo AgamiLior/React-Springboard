7a6ab28e4373ba295396dfe3d12505103c9bed0b
Lior Agami <lioragami@liors-mbp.lan>
2022-04-21 18:35:53 -0400

    adding unit 39.2

---
+++ springboard 39.2/part-1/App.js
@@ -0,0 +1,7 @@
+const App = () => 
+    <div>
+        <FirstComponent />
+        <NamedComponent name = "Lior" />
+    </div>
+
+ReactDOM.render(<App />, document.getElementById("root"))
---
+++ springboard 39.2/part-2/App.js
@@ -0,0 +1,5 @@
+const App = () => <div>
+    <Tweet username="username1" name="User1" date="2022-4-21" message="message 1" />
+    <Tweet username="username2" name="User2" date="2022-4-21" message="message 2" />
+    <Tweet username="username3" name="User3" date="2022-4-21" message="message 3" />
+</div>
